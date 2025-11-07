836327ede2f548ed5b67f67c90646d6b39c631a8
manojk765 <manojkoneti05@gmail.com>
2025-11-07 10:07:17 +0530

    Fix: added missing path import

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require("path");   
 const Rooms = require('./rooms');
 const cors = require('cors') ;
 
