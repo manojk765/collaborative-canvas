200846ddc04b2c93235b3558ee14da52bbe9529d
manojk765 <manojkoneti05@gmail.com>
2025-11-07 09:21:50 +0530

    Changes in Server

--- server/server.js
+++ server/server.js
@@ -93,4 +93,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+// server.listen(3000, () => console.log('Server running on port 3000'));
+
+// To:
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Server running on port ${port}`));
